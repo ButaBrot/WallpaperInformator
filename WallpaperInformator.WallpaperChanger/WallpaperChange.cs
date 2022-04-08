390c42409c35ac5b08f8e6cff06a1d5eaa3e50c1
Kraken <zuhause@home.de>
2022-04-08 20:13:24 +0200

    Projektdateien hinzufügen.

---
+++ WallpaperInformator.WallpaperChanger/WallpaperChange.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Win32;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.Shell;
+
+namespace WallpaperInformator.WallpaperChanger
+{
+    public interface IWallpaperChange
+    {
+        void SetMonitorWallpaper(int monitorIndex, string pathToWallpaper);
+    }
+
+    public class WallpaperChange : IWallpaperChange
+    {
+        private IDesktopWallpaper desktopWallpaper;
+
+        public WallpaperChange()
+        {
+            desktopWallpaper = (IDesktopWallpaper)new DesktopWallpaper();
+        }
+
+        public void SetMonitorWallpaper(int monitorIndex, string pathToWallpaper)
+        {
+            string monitorId = GetMonitorDevicePath((uint)monitorIndex);
+            SetWallpaper(pathToWallpaper,monitorId);
+        }
+        private void SetWallpaper(string pathToWallpaper, string monitorID)
+        {
+            desktopWallpaper.SetWallpaper(monitorID, pathToWallpaper);
+        }
+
+        private string GetMonitorDevicePath(uint monitorIndex)
+        {
+            desktopWallpaper.GetMonitorDevicePathAt(monitorIndex, out PWSTR monitorIdNative);
+            return monitorIdNative.ToString();
+        }
+    }
+}
